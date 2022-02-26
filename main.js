7f958a7e139238c804b9f0855ca03028d5ab37c4
sonndy <123dykai@gmail.com>
2022-02-26 14:37:47 -0500

    add new project to dcim section

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
     let dcimSection = document.querySelector(".DCIM");
     let experienceSection = document.querySelector(".experience");
 
+    webDevButton.classList.add("border");
 
 
     linksContainer.addEventListener("click", (e)=> {
@@ -20,17 +21,30 @@ window.addEventListener("DOMContentLoaded", ()=> {
             experienceSection.classList.remove("show");
             dcimSection.classList.add("show");
 
+            dcimButton.classList.add("border");
+            webDevButton.classList.remove("border");
+            experienceButton.classList.remove("border");
+
         } else if(e.target.textContent === "Web Development") {
             webDevSection.classList.remove("hide");
             experienceSection.classList.add("hide");
             dcimSection.classList.remove("show");
             experienceSection.classList.remove("show");
+
+            dcimButton.classList.remove("border");
+            webDevButton.classList.add("border");
+            experienceButton.classList.remove("border");
+
         } else if((e.target.textContent === "Experience")){
             webDevSection.classList.add("hide");
             experienceSection.classList.remove("hide");
             experienceSection.classList.add("show");
             dcimSection.classList.add("hide");
             dcimSection.classList.remove("show");
+
+            experienceButton.classList.add("border");
+            dcimButton.classList.remove("border");
+            webDevButton.classList.remove("border");
         }
     })
     
